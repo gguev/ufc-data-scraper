88608485de086fe313aa93cf325b3e6990966de0
Guillermo Guevara <guillermo1699@hotmail.com>
2025-11-06 15:38:47 -0500

    Modify events.ts types

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,19 +1,20 @@
+export interface StartTime {
+  dateTime: string | null
+  unix: number | null
+}
+
+export interface Location {
+  venue: string
+  locality: string | null
+  administrativeArea: string | null
+  country: string | null
+}
+
 export interface Event {
   eventName: string
   headline: string
-  mainCard: {
-    dateTime: string
-    unix: number
-  }
-  prelim: {
-    dateTime: string | null
-    unix: number | null
-  }
+  mainCard: StartTime
+  prelim: StartTime
   slug: string
-  location: {
-    venue: string
-    locality: string | null
-    administrativeArea: string | null
-    country: string | null
-  }
+  location: Location
 }
